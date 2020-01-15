17d9ecb37a1292a967f772ab223560fea8f85a91
Mishuni <miseon543@gmail.com>
2020-01-15 20:16:11 +0900

    Update 8 pm

--- Algorithm/Baekjoon/src/Bj10816_2.java
+++ Algorithm/Baekjoon/src/Bj10816_2.java
@@ -1,27 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
+
 public class Bj10816_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] num = new int[N];
-		for(int i=0; i<N; ++i) {
-			num[i] = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		int[] card = new int[20000001];
+		for (int i = 0; i < N; ++i) {
+			 ++card[sc.nextInt()+10000000] ;
 		}
 		int M = sc.nextInt();
-		int[] check = new int[M];
-		for(int i=0; i<M; ++i) {
-			check[i] = sc.nextInt();
-		}
-		Arrays.sort(num);
-		Arrays.sort(check);
-		int left = num[0];
-		int right = num[num.length - 1];
-		for(int m : check) {
-			int half = (left + right) / 2;
-		}
-
+		
+		for (int i = 0; i < M; ++i) {
+			sb.append(card[sc.nextInt()+10000000]+" ");
+		}		
+		System.out.println(sb.toString());
 	}
 
 }
