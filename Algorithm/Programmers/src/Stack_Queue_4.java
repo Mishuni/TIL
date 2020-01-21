86dd8e25d737bc0fe366e85f889a451bcb9d1e6b
Mishuni <miseon543@gmail.com>
2020-01-21 21:37:14 +0900

    Update Progrmmers

--- Algorithm/Programmers/src/Stack_Queue_4.java
+++ Algorithm/Programmers/src/Stack_Queue_4.java
@@ -1,34 +1,55 @@
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Stack_Queue_4 {
 
 	public static void main(String[] args) {
-		int[] progresses= {93,30,55}; 
-		int[] speeds = {1,30,5};
-		int[] release = new int[speeds.length];
-		
-		int cnt = 1 ;
-		int index= 0 ;
-		int tmp = (int) Math.ceil((double)(100-progresses[0]) / speeds[0]);
-		int pre = tmp;
-		
-		for(int i=1; i<progresses.length; ++i) {
-			tmp = (int) Math.ceil((double)(100-progresses[i]) / speeds[i]);
-			if(tmp>pre) {
-				pre = tmp ;
-				release[index++] = cnt;
-				cnt=1;
-			}else {
-				++cnt;
+		ArrayList<Node> test = new ArrayList<Node>();
+		int[] priorities = { 1, 2, 3, 1, 5 };
+		int location = 1;
+		int N = priorities.length;
+		test.add(new Node(priorities[0], 0));
+		for (int i = 1; i < N; ++i) {
+			test.add(new Node(priorities[i], i));
+		}
+		// Collections.sort(test);
+		Arrays.sort(priorities);
+
+		int i = N - 1;
+		int j = 0;
+		int order = 1;
+		while (i >= 0) {
+			if (j >= test.size()) {
+				j = 0;
+			}
+			Node tmp = test.get(j++);
+			if (tmp.value >= priorities[i]) {
+
+				test.remove(tmp);
+
+				if (tmp.idx == (location)) {
+
+					break;
+				}
+
+				++order;
+				--j;
+				--i;
 			}
 		}
-		release[index] = cnt;
-		int[] answer = new int[index+1];
-		for(int i=0; i<=index; ++i) {
-			answer[i] = release[i];
-		}
-		System.out.println(Arrays.toString(answer));
+		System.out.println(order);
+	}
+}
+
+class Node {
+
+	int value;
+	int idx;
+
+	Node(int v, int i) {
+		value = v;
+		idx = i;
 	}
 
 }
-
