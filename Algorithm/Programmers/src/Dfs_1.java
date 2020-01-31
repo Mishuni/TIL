a5be787ecaaf9e3ad959ec05dcf47b8dabbfcae3
Mishuni <miseon543@gmail.com>
2020-01-31 17:51:09 +0900

    Update algorithm

--- Algorithm/Programmers/src/Dfs_1.java
+++ Algorithm/Programmers/src/Dfs_1.java
@@ -1,38 +1,75 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
-
+import java.util.LinkedList;
 public class Dfs_1 {
-	public static ArrayList<Integer> prime ;
 	public static void main(String[] args) {
-		String numbers = "012377";
-		int cnt = 0 ;
-		prime = new ArrayList<Integer>();
-		prime.add(2);
-		int[] allNum = new int[numbers.length()];
-		
-		for(int i=0; i<allNum.length; ++i) {
-			allNum[i] = numbers.charAt(i)-'0';
-			System.out.println(check(allNum[i]));
-		}
-		Arrays.sort(allNum);
-
+	System.out.println(Solution.solution("2425"));
 	}
-	
-	public static boolean check(int num) {
-		if(num==2) return true;
-		boolean p = false;
-		for(int i=2; i<=num; ++i) {
-			for(int j=0; j<prime.size(); ++j) {
-				if(i%prime.get(j)==0) break;
-				if(j==prime.size()-1) {
-					prime.add(i);
-					if(i==num) {
-						p = true;
-					}
-				}
-			}
-		}
-		return p;
-	}
-
 }
+
+class Solution {
+    static boolean[] checked;
+    static int [] primeset;
+    static String[] numbers_arr;
+    public static int solution(String numbers) {
+        primeset=new int[10000000];
+        checked=new boolean[10000000];
+
+        for(int i=2;i*2<10000000;++i) {
+        	// check 가 되어 있으면 넘어가기
+        	// 작은 수의 배수이면 소수가 아니니까 true
+            if(!checked[i])
+            {
+                for(int j=i*2;j<10000000;j+=i) {
+                    checked[j]=true;
+                }
+            }
+        }
+        // 0과 1은 소수가 아니다
+        checked[0]=true;
+        checked[1]=true;
+        
+        numbers_arr=new String[numbers.length()+1];
+        for(int i=1;i<=numbers.length();++i){
+            numbers_arr[i]=numbers.substring(i-1,i);
+        }
+
+        for(int i=1;i<=numbers.length();++i){
+            int[] check = new int[numbers.length()+1];
+            LinkedList<String> rCom=new LinkedList<>();
+            rePermutation(numbers.length(),i,rCom,check);
+        }
+
+        long result=Arrays.stream(primeset).filter(x -> x==1).count();
+        int answer = (int)result;
+        return answer;
+    }
+    
+    private static void
+    rePermutation(int n, int r, LinkedList<String> rCom, int[] check) {
+        if(rCom.size() == r){
+            String result ="";
+            for(String i : rCom)
+                result+=i;
+            int comp=Integer.parseInt(result);
+            if(!checked[comp]){
+                primeset[comp]=1;
+            }
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            if(check[i] == 0){
+                check[i] = 1;
+                rCom.add(numbers_arr[i]);
+                rePermutation(n, r, rCom, check);
+                check[i] = 0;
+                rCom.removeLast();
+            }
+        }
+
+    }
+    }
+
+
+
---
+++ Algorithm/Programmers/src/Dfs_1_2.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+public class Dfs_1_2 {
+	public static void main(String[] args) {
+	System.out.println(Solution.solution("2425"));
+	}
+}
+
+
