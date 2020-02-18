b9a127b35e7fa7bae236dd45d293d9c77b68cae9
Mishuni <miseon543@gmail.com>
2020-02-19 02:13:03 +0900

    Baekjoon

--- Algorithm/Baekjoon/src/Bj15649.java
+++ Algorithm/Baekjoon/src/Bj15649.java
@@ -2,42 +2,46 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Bj15649 {
-	static boolean[] checked ;
-	static LinkedList<Integer> list;
+
+	static int[] arr;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		checked = new boolean[n+1];
-		list = new LinkedList<Integer>();
-		dfs(n,m);
+		// checked = new boolean[n+1];
+		// list = new LinkedList<Integer>();
+		arr = new int[m];
+		dfs(n, m);
 		sc.close();
 	}
-	
-	public static void dfs( int n, int m) {
-		
+
+	public static void dfs(int n, int m) {
+
 		// 기저 사례
-		if(m==0) {
-			for(int i=0; i<list.size(); ++i ) {
-				System.out.print(list.get(i)+" ");
+		if (m == 0) {
+			for (int i = 0; i < arr.length; ++i) {
+				System.out.print(arr[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
 
 		// 완전 탐색
-		for( int i=1; i<=n; ++i ) {
-			//if(!checked[i]) {
-				// 아직 지나가지 않은 상태
-				//checked[i] = true;
-				list.add(i);
-				dfs(n,m-1);
-				list.removeLast();
-				//checked[i] = false;
-			//}
-			
+		for (int i = 1; i <= n; ++i) {
+			if (arr.length - m > 0) {
+				if (arr[arr.length - m - 1] <= i) {
+					arr[arr.length - m] = i;
+				} else {
+					continue;
+				}
+			}
+			else {
+				arr[0] = i;
+			}
+			dfs(n, m - 1);
 		}
-		
+
 	}
 
 }
