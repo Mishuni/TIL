0834e0bdc622c7630b30a70f407e0ed68bf77f73
Mishuni <miseon543@gmail.com>
2020-02-24 17:32:10 +0900

    Update programmers

--- Algorithm/Programmers/src/Greedy_2.java
+++ Algorithm/Programmers/src/Greedy_2.java
@@ -3,19 +3,31 @@ public class Greedy_2 {
 
 	public static void main(String[] args) {
 		String name = "JEROEN";
-		int answer = 0 ;
-		// A는 아무것도 안해도 된다.
 		
+		// JEAAAEE -> 오1 왼3
+		// JEAEEEE -> 왼6
+		// JEEAEEE -> 왼6
+		// JAAAAAE -> 왼1
+		// JEAAAAE -> 오1 왼2
+		// AAJEEEE -> 왼5
+		// AJAJAJA -> 오5
+		// JAEEAJJ -> 왼5
+		// AAAEAAA -> 오3
+		int answer = 0 ;
+
 		// 상하는 A와 차이 절댓값
+		// a b c d e f g h i j k l m
+		// n o p q r s t u v w x y z
 		for(int i=0; i<name.length(); ++i) {
 			if(name.charAt(i)<='M') {
 				answer += Math.abs(name.charAt(i)-'A');
 			}
 			else {
-				answer += Math.abs(name.charAt(i)-'Z'+1);
+				answer += Math.abs(name.charAt(i)-'Z')+1;
 			}
+			System.out.println(answer);
 		}
-		System.out.println(answer);
+		//System.out.println(answer);
 	}
 
 }
