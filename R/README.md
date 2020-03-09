9c841b8772996f259476330627afcc35e00dced6
Mishuni <miseon543@gmail.com>
2020-03-09 16:15:08 +0900

    Update R

--- R/README.md
+++ R/README.md
@@ -2,19 +2,19 @@
 
 ## 환경 설정(Windows)
 
-1) R Tool
+### 1) R Tool
 
 https://cran.r-project.org/ > [Download R for Windows](https://cran.r-project.org/bin/windows/) > base 클릭 > 설치 후 실행 후 defalut 형태로 설치 **컴퓨터 로그인 id 가 한글로 되어 있으면 문제가 발생 할 수 있음**
 
-2) 개발 IDE
+### 2) 개발 IDE
 
 [R Studio](https://rstudio.com/) > product > R Studio > [R Studio Desktop](https://rstudio.com/products/rstudio/download/) > RStudio Desktop free 버전 다운로드 > default 형태로 설치
 
-3) Project
+### 3) Project
 
 File > new Project > New Project > 프로젝트 이름, 워크스페이스 위치 설정
 
-4) Encoding
+### 4) Encoding
 
 Tools > global Options (모든 프로젝트에 해당) > Code > Soft-wrap R source files (줄바꿈 처리) > Saving > encoding쪽에 change > UTF-8 로 바꿔서 저장
 
@@ -47,7 +47,22 @@ Tools > global Options (모든 프로젝트에 해당) > Code > Soft-wrap R sour
 
   ex) 3 / 0 #Inf
 
+**데이터 타입의 우선 순위**: 기본적으로 사용하는 자료구조는 vector 이므로 우선 순위가 있음 
+
+​	***character > complex > numeric > logical***
+
+**자료구조**: 
+
+* homogeneous (같은 종류) : 같은 종류의 데이터 타입들만 있는 구조  
+  1. vector : 1차원 선형구조, 순서 존재
+  2. matrix : 2차원 구조, 순서 존재
+  3. array : 3차원 이상의 같은 데이터 타입으로만 이루어진 구조
+* heterogeneous (다른 종류) : 데이터 타입들이 섞여 있는 구조 
+  1. list (중첩 자료 구조) : 1차원 선형구조, 순서 존재, 실제로 저장되는 구조는 map 구조, map 과 배열을 짬뽕시킨 배열 (list 안에는 vector, matix, numeric 다 들어올 수 있음)
+  2. data frame : 2차원 테이블 구조 
+
 **경로**: Linux 기반이라 역슬래시(|)가 아니라 (/)기반으로 경로를 나눔
 
 **RData 파일**: R 전용 데이터 파일(.rda),  다른 파일들에 비해 R에서 일고 쓰는 속도가 빠르고 용량이 작음, R에서 분석 작업시에 RData파일을 이용하고, R을 사용하지 않는 사람과 파일을 주고받을 시에 CSV파일을 사용
 
+참고사이트 : [R documentation](https://www.rdocumentation.org/)
--- R/R_Workspace/R_Lecture/Chapter_01.R
+++ R/R_Workspace/R_Lecture/Chapter_01.R
@@ -1,16 +1,24 @@
-# 여러 줄 주석 설정은
-# ctrl + shift + c 
+# 여러 줄 주석 설정은: ctrl + shift + c 
+# 스크립트 실행은 (interpreter 형태): ctrl + enter
 
-# 스크립트 실행은 (interpreter 형태)
-# crtl + enter
-
-# Weak Type
+# R package
+## pakage 설치
+remove.packages("ggplot2")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("readxl")
+## pakage 로딩
 library(ggplot2)
-library(readxl)
+library(dplyr)
+require(readxl)
+# path 출력 (base pakage에 들어 있는 함수)
+# 안에 경로 문자열 넣어주면 lib 설치 경로 바꿀 수 있음
+.libPaths() # 출력되는 것 중 1번 경로에 라이브러리가 설치됨
+
 A <- 100 ; B <- 300; C=400; D<- 500 ->E
 a <-c(1:4)
 500 -> R
-x <- c("a","a","b","c")
+x <- c("z","c")
 qplot(x)
 qplot(data=mpg,x=drv, y=hwy)
 qplot(data=mpg,x=drv, y=hwy, geom="boxplot", colour = drv)
@@ -69,8 +77,6 @@ str(exam)
 summary(exam)
 mpg <- as.data.frame(ggplot2::mpg)
 tail(mpg)
-dim(mpg)
-str(mpg)
 ?mpg
 summary(mpg)
 
@@ -80,3 +86,77 @@ is.numeric(a) # 데이터가 수치형인지
 is.double(B) # 데이터가 실수인지
 is.integer(10L) # 데이터가 정수인지
 is.integer(10) 
+
+# vector 는 데이터 타입이 모두 같아야함
+# 위 같은 경우에 logical 이 우선순위가 더 낮으므로
+# FALSE 는 0으로 바뀜
+myvar = c(19,20,30,40,FALSE) 
+# 밑은 다 문자열로 바뀜
+myvar = c(3+2i,"a",123,TRUE)
+myvar = "100"
+as.numeric(myvar) #as계열 이용한 형변환(casting)
+
+df_raw <- data.frame(var1=c(1,2,1),
+                     var2=c(2,3,2))
+df_new <- df_raw
+df_new <- rename(df_new,v2=var2) #var2 를 v2 로 수정
+df_new$var_sum <- df_new$var1 + df_new$v2
+df_new
+mpg$total <- (mpg$cty + mpg$hwy)/2
+head(mpg)
+summary(mpg$total)
+hist(mpg$total)
+mpg$test <- ifelse(mpg$total >=20 , "pass", "fail")
+table(mpg$test)
+mpg$grade <- ifelse(mpg$total>=30,"A",
+                    ifelse(mpg$total>=20,"B","C"))
+table(mpg$grade)
+qplot(mpg$grade)
+
+mds <- as.data.frame(ggplot2::midwest)
+head(mds)
+mds <- rename(mds, total=poptotal)
+mds <- rename(mds, asian=popasian)
+mds$per_asian <- ((mds$asian/mds$total)*100)
+asian_dst_mean = mean(mds$per_asian)
+mds$asian_lot <- ifelse(mds$per_asian>=asian_dst_mean,"large",
+                        "small")
+qplot(mds$asian_lot)
+
+## 자료구조
+# vector : index 시작은 1 
+myvar = c(100) # scala
+# numeric 데이터만 사용할 수 있는 단조증가,감소 vector생성
+myvar = 2:10 # 둘 다 포함한 범위에서 생성
+myvar = 8.4:2
+myvar = seq(1, 10, by = 2)
+myvar = seq(from=10, to=3, by=-3)
+myvar = rep(1:3,times=3)
+myvar = rep(1:3, each=3)
+length(myvar)
+myvar[1]
+myvar[length(myvar)]
+
+result <- myvar[3:7] # slicing
+result <- myvar[c(1,4,7)] # fancy indexing
+myvar[-1] # 1번 요소 빼고 나머지
+myvar[-(3:5)] # 3,4,5 요소 빼고 나머지
+
+### vector 데이터에 이름 붙이기
+myvar <- seq(10,50,by=10)
+names(myvar) = c("a","b","c","d")
+names(myvar) # 각 요소의 이름 출력
+myvar
+myvar["a"]
+
+# vector 연산
+myvar1 = 1:3
+myvar2 = 4:6
+result = myvar1 - myvar2
+result = myvar1 * myvar2
+myvar3 = 4:10
+# 크기가 다르면?
+# 작은 벡터를 큰 벡터에 맞춤
+# 작은 벡터를 순환시켜서 생성 [recycle rule]
+result = myvar1 * myvar3
+
