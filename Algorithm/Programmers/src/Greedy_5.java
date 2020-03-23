0bfcef448670937f3c7ab1fc25f9c6a6167a3c1c
Mishuni <miseon543@gmail.com>
2020-03-23 17:51:21 +0900

    Update greedy_5

--- Algorithm/Programmers/src/Greedy_5.java
+++ Algorithm/Programmers/src/Greedy_5.java
@@ -4,38 +4,23 @@ public class Greedy_5 {
 
 	public static void main(String[] args) {
 		
-		int[][] routes = {{-20,15}, {-14,-5}, {-18,-13}, {-5,-3},{-5,-4},{16,18}};
-		boolean[] check = new boolean[routes.length];
-		int[] path = new int[41];
-		int max = 0;
-		int maxInt = 0 ;
-		int cnt = 0 ;
+		int[][] routes = {{-20,15}, {-14,-5}, {-18,-13}, {-5,-3}};
+		// 끝나는 시간 순 대로 오름차순 정렬
+		Arrays.sort(routes, (a,b)-> Integer.compare(a[1], b[1]));
+		int cnt = 0;
 		
-		// 1. 겹치는 구간 찾기
-		
+		int min = Integer.MIN_VALUE;
 		for(int[] route : routes) {
-			for(int i=route[0]; i<=route[1]; ++i) {
-				if(++path[i+20]>max) {
-					max = path[i+20];
-					maxInt = i+20;
-				}
+			if(min <  route[0] ) {
+				// 전 거의 끝나는 시간 보다 시작 시간이 큰 경우
+				// 안 겹치니까 변경하고, 숫자 더하기
+				min = route[1];
+				++cnt;
 			}
 		}
-		
-		// 2. 제일 많이 겹치는 구간 부터 계산 하기
-		for(int r=0; r< routes.length; ++r) {
-			int[] route = routes[r];
-			if( !check[r]&&route[1]<=maxInt) {
-				for(int i=route[0]; i<=route[1]; ++i) {
-					--path[i+20];
-				}
-				check[r] = true;
-			}
-		}
-		++cnt;
-		
-		System.out.println(Arrays.toString(path));
-		
+		System.out.println(cnt);
+				
 	}
+		
 
 }
