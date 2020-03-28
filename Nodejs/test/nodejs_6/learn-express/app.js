5d8ead67e19c444253524922c9075e84632f6c4f
Mishuni <miseon543@gmail.com>
2020-03-29 00:54:14 +0900

    Update Nodejs

--- Nodejs/test/nodejs_6/learn-express/app.js
+++ Nodejs/test/nodejs_6/learn-express/app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 //app.engine('html', engines.mustache);
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,20 +20,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.use(cookieParser('secret code'));
 app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: 'secret code',
     cookie: {
-        httpOnly: true,
-        secure: false,
+      httpOnly: true,
+      secure: false,
     },
-}));
-app.use(flash());
+  }));
+  app.use(flash());
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// 404 처리 미들웨어
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // 에러 핸들러
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
 
 module.exports = app;
