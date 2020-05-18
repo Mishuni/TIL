70efb63b9573c8ee539b1edbdb5c79c7716f57a6
Mishuni <miseon543@gmail.com>
2020-05-18 09:59:19 +0900

    Complete 1767

--- Algorithm/Practice/src/Es_02.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Es_02 {
-
-	public static void main(String[] args) {
-			// [3,2,4,8,7]
-		//int a = []
-	}
-
-}
--- Algorithm/Practice/src/Es_04.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Es_04 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
